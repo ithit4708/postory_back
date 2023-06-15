6d70b85ffafb78469ae49c00550ef7fa654ccaab
ithit4708 <intae4708@naver.com>
2023-06-15 15:01:37 +0900

    프로젝트 생성 및 초기 설정
    1. .gitIgnore 파일내용 삽입
    2. Spring Boot 초기 설정
    3. Spring Sedcurity 초기 설정

---
+++ src/main/java/com/jungsuk_2_1/postory/PostoryApplication.java
@@ -0,0 +1,13 @@
+package com.jungsuk_2_1.postory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class PostoryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PostoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jungsuk_2_1/postory/PostoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungsuk_2_1.postory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
