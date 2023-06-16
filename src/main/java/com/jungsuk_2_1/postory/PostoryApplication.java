9d085f97226ee1159fb3dd7e60598657f838a2dd
Minkooang <leading.advertisement@gmail.com>
2023-06-16 20:10:30 +0900

    유저 인증,인가 관련 클래스 추가 및 불필요한 코드 제거
    1. Spring Security와 Jwt를 적용한 필터 클래스 추가
    2. Spring Security 설정을 위한 Config 클래스 추가
    3. AccessToken 생성을 위한 클래스 추가
    4. Spring Boot 실행파일에 사용하지 않는 코드 제거

--- src/main/java/com/jungsuk_2_1/postory/PostoryApplication.java
+++ src/main/java/com/jungsuk_2_1/postory/PostoryApplication.java
@@ -2,9 +2,8 @@ package com.jungsuk_2_1.postory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class PostoryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PostoryApplication.class, args);
