f6d44f58c56e4de3d21f1bb1ea7efea182930b30
Minkooang <leading.advertisement@gmail.com>
2023-06-23 20:52:08 +0900

    CORS 설정 관련 메서드 추가
    1. CorsConfigurationSource 관련 메서드 추가

--- src/main/java/com/jungsuk_2_1/postory/config/WebSecurityConfig.java
+++ src/main/java/com/jungsuk_2_1/postory/config/WebSecurityConfig.java
@@ -10,7 +10,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
@@ -46,6 +51,18 @@ public class WebSecurityConfig {
                 CorsFilter.class
         );
         return http.build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD","GET","POST","PUT","DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization","Cache-Control","Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
     }
 }
 //WebSecurityConfigurerAdapter - Deprecated
