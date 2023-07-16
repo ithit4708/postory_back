95cb8338693e3db549e452f3b096bcd4d8963756
dancingKim <rightlightfg@gmail.com>
2023-07-17 02:06:33 +0900

    update: Update uploading file feature
    1. upload 기능이 파일을 AWS S3에 올리게 수정

--- src/main/java/com/jungsuk_2_1/postory/controller/FileController.java
+++ src/main/java/com/jungsuk_2_1/postory/controller/FileController.java
@@ -4,27 +4,39 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.jungsuk_2_1.postory.dto.FileDto;
 import com.jungsuk_2_1.postory.service.FileService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Value;
+
 //import com.spring.multipart.dto.FoodDto;
 
 @RestController
 @RequestMapping("/file")
+@RequiredArgsConstructor
 public class FileController {
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
     private FileService fileService;
 
     @Autowired
-    FileController(FileService fileService){
+    FileController(AmazonS3Client amazonS3Client, FileService fileService){
+        this.amazonS3Client = amazonS3Client;
         this.fileService = fileService;
     }
     @PostMapping("/uploadFiles")
     public ResponseEntity<Object> uploadFiles(MultipartFile[] multipartFiles) {
+
         String UPLOAD_PATH = "/Users/ho/Workspace/sanwangjapki/postory_front/public/images";
 
         List<Object> urls = new ArrayList<>();
@@ -39,13 +51,23 @@ public class FileController {
                 originName = originName.substring(0, originName.lastIndexOf(".")); // ex) 파일
                 long fileSize = file.getSize(); // 파일 사이즈
 
+                ObjectMetadata metadata = new ObjectMetadata();
+                metadata.setContentType(fileExtension);
+                metadata.setContentLength(fileSize);
+                System.out.println("bucket = "+ bucket);
+//                String fileUrl = "https://" + bucket + "/"+fileId + "." + fileExtension;
+
+                String fileUrl = "https://postory-image.s3.ap-northeast-2.amazonaws.com/src/"+ fileId + "."+ fileExtension;
+                System.out.println("fileUrl = " + fileUrl);
+                amazonS3Client.putObject(bucket,fileId+"."+fileExtension, file.getInputStream(),metadata);
+
                 File fileSave = new File(UPLOAD_PATH, fileId + "." + fileExtension); // ex) fileId.jpg
                 if(!fileSave.exists()) { // 폴더가 없을 경우 폴더 만들기
                     fileSave.mkdirs();
                 }
 
                 file.transferTo(fileSave); // fileSave의 형태로 파일 저장
-                urls.add(fileId + "."+fileExtension);
+                urls.add(fileUrl);
 
                 System.out.println("fileId= " + fileId);
                 System.out.println("originName= " + originName);
@@ -78,13 +100,25 @@ public class FileController {
             originName = originName.substring(0, originName.lastIndexOf(".")); // ex) 파일
             long fileSize = file.getSize(); // 파일 사이즈
 
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(fileSize);
+            System.out.println("bucket = "+ bucket);
+//            https://postory-image.s3.ap-northeast-2.amazonaws.com/src/16895093343242601.png
+
+            String fileUrl = "https://postory-image.s3.ap-northeast-2.amazonaws.com/src/"+ fileId + "."+ fileExtension;
+            System.out.println("fileUrl = " + fileUrl);
+            amazonS3Client.putObject(bucket,fileId+"."+ fileExtension,file.getInputStream(),metadata);
+
             File fileSave = new File(UPLOAD_PATH, fileId + "." + fileExtension); // ex) fileId.jpg
             if(!fileSave.exists()) { // 폴더가 없을 경우 폴더 만들기
                 fileSave.mkdirs();
             }
 
             file.transferTo(fileSave); // fileSave의 형태로 파일 저장
-            url = fileId + "." + fileExtension;
+//            url = fileId + "." + fileExtension;
+            url = fileUrl + "." + fileExtension;
+
 //
             System.out.println("fileId= " + fileId);
             System.out.println("originName= " + originName);
